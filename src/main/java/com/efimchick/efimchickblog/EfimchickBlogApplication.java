5ba50eb80d1f01b28e626d7182ba9795cccbf2cc
Jerome <thejerome@mail.ru>
2017-08-09 16:06:35 +0300

    Initial commit

---
+++ src/main/java/com/efimchick/efimchickblog/EfimchickBlogApplication.java
@@ -0,0 +1,12 @@
+package com.efimchick.efimchickblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfimchickBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfimchickBlogApplication.class, args);
+	}
+}
